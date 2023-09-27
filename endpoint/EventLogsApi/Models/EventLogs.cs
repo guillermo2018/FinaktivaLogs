b4b30829dd316f18b1fc39bbe615516f0c5d4259
guillermo betancur <guillermobatancur@gmail.com>
2023-09-26 21:03:04 -0500

    Add existing project files to Git

---
+++ endpoint/EventLogsApi/Models/EventLogs.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventLogsApi.Models
+{
+    public class EventLogs
+    {
+        [Key]
+        public int EventID { get; set; }
+        public string EventType { get; set; }
+        [Required(ErrorMessage = "The EventDescription field is required.")]
+        public string EventDescription { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
